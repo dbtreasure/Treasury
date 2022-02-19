4df30651d550306c50e36f4af34543720f703b90
Dan Treasure <beepboop3762@protonmail.com>
2022-02-19 13:28:24 -0700

    Initial Commit

---
+++ Treasury/TreasuryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreasuryApp.swift
+//  Treasury
+//
+//  Created by Daniel Treasure on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct TreasuryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
